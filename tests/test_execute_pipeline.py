26c8bcc828d0b603583e121b5c7155ba43ef7c15
Leo Schick <leonhard.schick@gmail.com>
2022-07-18 07:55:19 +0200

    add pipeline failed test

--- tests/test_execute_pipeline.py
+++ tests/test_execute_pipeline.py
@@ -6,7 +6,10 @@ import mara_db.config
 patch(mara_db.config.databases)(lambda: {})
 
 
-def test_execute_without_db():
+def test_execute_without_db_success():
+    """
+    A simple test pipeline with a success run not using the mara database for logging.
+    """
     from mara_pipelines.commands.python import RunFunction
     from mara_pipelines.pipelines import Pipeline, Task
     from mara_pipelines.ui.cli import run_pipeline
@@ -24,3 +27,26 @@ def test_execute_without_db():
              commands=[RunFunction(function=command_function)]))
 
     assert run_pipeline(pipeline)
+
+
+def test_execute_without_db_failed():
+    """
+    A simple test pipeline with a failed run not using the mara database for logging.
+    """
+    from mara_pipelines.commands.python import RunFunction
+    from mara_pipelines.pipelines import Pipeline, Task
+    from mara_pipelines.ui.cli import run_pipeline
+
+    pipeline = Pipeline(
+        id='test_execute_without_db',
+        description="Tests if a pipeline can be executed without database")
+    
+    def command_function() -> bool:
+        return False
+
+    pipeline.add(
+        Task(id='run_python_function',
+             description="Runs a sample python function",
+             commands=[RunFunction(function=command_function)]))
+
+    assert not run_pipeline(pipeline)
